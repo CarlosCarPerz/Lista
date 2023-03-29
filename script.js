ef97ec2955e2fa98d0d15435031f4506e9c7ac93
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2023-03-28 19:19:50 -0600

    Funcionamiento de botones listo, cambio de colores

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
-( () => { 
-    const btn = document.querySelector("[data-form-btn]");
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
 
-    const createTask = (evento) => {
-        evento.preventDefault();
-        const input = document.querySelector("[data-form-input]");
-        const value = input.value;
-        const list = document.querySelector("[data-list]");
-        const task = document.createElement("li");
-        const taskContent = document.createElement("div");
-        const textTitle = document.createElement("span");
+const btn = document.querySelector("[data-form-btn]");
 
-        textTitle.classList.add("task");
-        task.classList.add("card");
-        
-        taskContent.appendChild(checkComplete());
-        taskContent.appendChild(textTitle);
-        task.appendChild(taskContent);
-        task.appendChild(deleteIcon());
-        list.appendChild(task);
-        
-        
-        textTitle.innerText = value;
-        input.value = "";
-    }
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    const taskContent = document.createElement("div");
+    const textTitle = document.createElement("span");
 
-    btn.addEventListener("click", createTask);
-
-    const checkComplete = () => {
-        const i = document.createElement("i");
-        i.classList.add("far", "fa-check-square", "icon");
-        i.addEventListener("click", completeTask);
-        return i;
-    }
-
-    const completeTask = (event) => {
-        const element = event.target;
-        element.classList.toggle('fas');
-        element.classList.toggle('completeIcon');
-        element.classList.toggle('far');
-    }
-
-    const deleteIcon = () => {
-        const i = document.createElement("i");
-        i.classList.add ("fas", "fa-trash-alt", "trashIcon", "icon");
-        i.addEventListener("click", deleteTask);
-        return i;
-    }
-
-    const deleteTask = () => {
-        console.log("delete task");
-    }
+    textTitle.classList.add("task");
+    task.classList.add("card");
     
-}) ()
+    taskContent.appendChild(checkComplete());
+    taskContent.appendChild(textTitle);
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
+    list.appendChild(task);
+    
+    
+    textTitle.innerText = value;
+    input.value = "";
+}
+
+btn.addEventListener("click", createTask);
