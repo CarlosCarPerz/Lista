577fb77bf7d02dda3cfa5c424f491acb6e6a9bcf
Carlos Carvajal <97265469+CarlosCarPerz@users.noreply.github.com>
2023-04-03 13:07:53 -0600

    Omite las tareas vacias y diseño responsivo

--- script.js
+++ script.js
@@ -6,24 +6,31 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    const value = input.value;
+    const value = obtenerValorInput(input);
     const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    const taskContent = document.createElement("div");
-    const textTitle = document.createElement("span");
+    if (value) {
+        const task = document.createElement("li");
+        const taskContent = document.createElement("div");
+        const textTitle = document.createElement("span");
 
-    textTitle.classList.add("task");
-    task.classList.add("card");
-    
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(textTitle);
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-    
-    
-    textTitle.innerText = value;
-    input.value = "";
+        textTitle.classList.add("task");
+        task.classList.add("card");
+        
+        taskContent.appendChild(checkComplete());
+        taskContent.appendChild(textTitle);
+        task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
+        
+        textTitle.innerText = value;
+        input.value = "";
+        }
 }
+
+function obtenerValorInput(inputElemento) {
+    if (inputElemento.value.trim() !== '') {
+        return inputElemento.value;
+        }
+    }
 
 btn.addEventListener("click", createTask);
